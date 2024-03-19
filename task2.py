a26d043e0c48781d38ccf61bd470707144eb91c1
danimir <nexe1054@yandex.ru>
2024-03-19 10:11:36 +0300

    add 2 tasks

---
+++ task2.py
@@ -0,0 +1,14 @@
+fibonacci_nums = [1, 2]
+while True:
+    if fibonacci_nums[-1] >= 4*10**6:
+        fibonacci_nums.pop(-1)
+        break
+    fibonacci_nums.append(fibonacci_nums[-1] + fibonacci_nums[-2])
+
+print(sum([num for num in fibonacci_nums if num%2==0]))
+
+
+# def gen_fibonacci(limit: int):
+#     pass
+#
+# print(sum(n for n in gen_fibonacci(4 * 10 ** 6) if n%2 == 0))
